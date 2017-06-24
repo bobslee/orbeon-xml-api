00010477aa988f834c43863405f6739621bbaedc
Bob Leers <bobleers@fastmail.fm>
2017-06-24 15:03:17 +0200

    Runner constructor in progress.

--- runner.py
+++ runner.py
@@ -4,7 +4,22 @@ from utils import generate_xml_root
 
 class Runner:
 
-    def __init__(self, xml, builder_xml, lang='en'):
+    def __init__(self, xml, builder=None, builder_xml=None, lang='en'):
+        """
+        @param builder Builder
+        @param builder_xml str
+        """
+
+        if builder and builder_xml:
+            raise Exception("Constructor accepts either builder or builder_xml.")
+
+        if builder:
+            assert isinstance(builder, Builder)
+        elif builder_xml:
+            assert isinstance(builder_xml, bytes)
+        else:
+            raise Exception("Provide either the argument: builder or builder_xml.")
+
         self.xml = xml
         self.builder_xml = builder_xml
         self.lang = lang
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -11,7 +11,31 @@ class RunnerTestCase(unittest.TestCase):
     def setUp(self):
         super(RunnerTestCase, self).setUp()
 
-        runner_xml = xml_from_file('tests/data', 'test_controls_runner.xml')
-        builder_xml = xml_from_file('tests/data', 'test_controls_builder.xml')
+        self.runner_xml = xml_from_file('tests/data', 'test_controls_runner.xml')
+        self.builder_xml = xml_from_file('tests/data', 'test_controls_builder.xml')
 
-        self.runner = Runner(runner_xml, builder_xml)
+        self.runner = Runner(self.runner_xml, None, self.builder_xml)
+
+    def test_constructor(self):
+        self.assertRaisesRegex(
+            Runner(self.runner_xml)
+        )
+
+        self.assertRaisesRegex(
+            Runner(self.runner_xml, self.builder_xml)
+        )
+
+        self.assertRaisesRegex(
+            Runner(self.runner_xml, self.builder)
+        )
+
+        self.assertRaisesRegex(
+            Runner(self.runner_xml, self.builder_xml, self.builder)
+        )
+
+        # Ok tests
+        runner = Runner(self.runner_xml, None, self.builder_xml)
+        self.assertIsInstance(runner, Runner)
+
+        runner = Runner(self.runner_xml, self.builder)
+        self.assertIsInstance(runner, Runner)
