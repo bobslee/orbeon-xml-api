8a7c5d022b2dea1161a23c25a5add1169f31a1b5
Bob Leers <b.leers@open2bizz.nl>
2017-10-09 13:23:59 +0200

    Unit test: US Phone Number (typed StringControl)

--- orbeon_xml_api/tests/controls/test_us_phone.py
+++ orbeon_xml_api/tests/controls/test_us_phone.py
@@ -1,9 +1,44 @@
 from . import CommonTestCase
 
+from ..controls import StringControl
+
 
 class UsPhoneTestCase(CommonTestCase):
 
-    def test_us_phone(self):
-        us_phone = self.builder.controls['us-phone']
-        self.assertEqual(us_phone.element.label, 'US Phone Number')
-        self.assertEqual(us_phone.element.hint, 'US phone number field')
+    def setUp(self):
+        super(UsPhoneTestCase, self).setUp()
+        self.control = self.builder.controls['us-phone']
+
+    def test_control(self):
+        self.assertIsInstance(self.control, StringControl)
+
+    def test_builder_bind(self):
+        self.assertEqual(self.control.bind.id, 'us-phone-bind')
+        self.assertEqual(self.control.bind.name, 'us-phone')
+
+    def test_builder_parent(self):
+        self.assertEqual(self.control.parent.bind.id, 'typed-controls-bind')
+        self.assertEqual(self.control.parent.bind.name, 'typed-controls')
+        self.assertEqual(self.control.parent.element.label, 'Typed Controls')
+
+    def test_builder_form(self):
+        self.assertEqual(self.control.label, 'US Phone Number')
+        self.assertEqual(self.control.hint, 'US phone number field')
+        self.assertEqual(self.control.alert, None)
+
+        self.assertEqual(self.control.element.label, 'US Phone Number')
+        self.assertEqual(self.control.element.hint, 'US phone number field')
+
+        # Doesn't exist, but shouldn't raise Exception
+        self.assertEqual(self.control.element.alert, None)
+
+    def test_builder_form_default_value(self):
+        self.assertEqual(self.control.default_raw_value, '5555555555')
+        self.assertEqual(self.control.default_value, '5555555555')
+
+    def test_runner_value(self):
+        self.assertEqual(self.runner.get_value('us-phone'), '1234567890')
+
+    def test_runner_form(self):
+        self.assertEqual(self.runner.form.usphone.label, 'US Phone Number')
+        self.assertEqual(self.runner.form.usphone.value, '1234567890')
