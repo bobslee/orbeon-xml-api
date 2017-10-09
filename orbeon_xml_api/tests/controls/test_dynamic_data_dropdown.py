3ae6e86a0b90b94e51e252c68697388483c47c61
Bob Leers <b.leers@open2bizz.nl>
2017-10-09 12:58:28 +0200

    Unit tests: Email (control) and Dynamic Data Dropdown (control).

--- orbeon_xml_api/tests/controls/test_dynamic_data_dropdown.py
+++ orbeon_xml_api/tests/controls/test_dynamic_data_dropdown.py
@@ -1,5 +1,7 @@
 from . import CommonTestCase
 
+from ..controls import StringControl
+
 
 class DynamicDataDropdownTestCase(CommonTestCase):
 
@@ -7,3 +9,41 @@ class DynamicDataDropdownTestCase(CommonTestCase):
         dynamic_data_dropdown = self.builder.controls['dynamic-data-dropdown']
         self.assertEqual(dynamic_data_dropdown.element.label, 'Dynamic Data Dropdown')
         self.assertEqual(dynamic_data_dropdown.element.hint, None)
+
+    def setUp(self):
+        super(DynamicDataDropdownTestCase, self).setUp()
+        self.control = self.builder.controls['dynamic-data-dropdown']
+
+    def test_control(self):
+        self.assertIsInstance(self.control, StringControl)
+
+    def test_builder_bind(self):
+        self.assertEqual(self.control.bind.id, 'dynamic-data-dropdown-bind')
+        self.assertEqual(self.control.bind.name, 'dynamic-data-dropdown')
+
+    def test_builder_parent(self):
+        self.assertEqual(self.control.parent.bind.id, 'selection-controls-bind')
+        self.assertEqual(self.control.parent.bind.name, 'selection-controls')
+        self.assertEqual(self.control.parent.element.label, 'Selection Controls')
+
+    def test_builder_form(self):
+        self.assertEqual(self.control.label, 'Dynamic Data Dropdown')
+        self.assertEqual(self.control.hint, None)
+        self.assertEqual(self.control.alert, None)
+
+        self.assertEqual(self.control.element.label, 'Dynamic Data Dropdown')
+        self.assertEqual(self.control.element.hint, None)
+
+        # Doesn't exist, but shouldn't raise Exception
+        self.assertEqual(self.control.element.alert, None)
+
+    def test_builder_form_default_value(self):
+        self.assertEqual(self.control.default_raw_value, 'ca')
+        self.assertEqual(self.control.default_value, 'ca')
+
+    def test_runner_value(self):
+        self.assertEqual(self.runner.get_value('dynamic-data-dropdown'), 'de')
+
+    def test_runner_form(self):
+        self.assertEqual(self.runner.form.dynamicdatadropdown.label, 'Dynamic Data Dropdown')
+        self.assertEqual(self.runner.form.dynamicdatadropdown.value, 'de')
--- orbeon_xml_api/tests/controls/test_email.py
+++ orbeon_xml_api/tests/controls/test_email.py
@@ -1,5 +1,7 @@
 from . import CommonTestCase
 
+from ..controls import StringControl
+
 
 class EmailTestCase(CommonTestCase):
 
@@ -7,3 +9,41 @@ class EmailTestCase(CommonTestCase):
         email = self.builder.controls['email']
         self.assertEqual(email.element.label, 'Email Address')
         self.assertEqual(email.element.hint, 'Email field with validation')
+
+    def setUp(self):
+        super(EmailTestCase, self).setUp()
+        self.control = self.builder.controls['email']
+
+    def test_control(self):
+        self.assertIsInstance(self.control, StringControl)
+
+    def test_builder_bind(self):
+        self.assertEqual(self.control.bind.id, 'email-bind')
+        self.assertEqual(self.control.bind.name, 'email')
+
+    def test_builder_parent(self):
+        self.assertEqual(self.control.parent.bind.id, 'typed-controls-bind')
+        self.assertEqual(self.control.parent.bind.name, 'typed-controls')
+        self.assertEqual(self.control.parent.element.label, 'Typed Controls')
+
+    def test_builder_form(self):
+        self.assertEqual(self.control.label, 'Email Address')
+        self.assertEqual(self.control.hint, 'Email field with validation')
+        self.assertEqual(self.control.alert, None)
+
+        self.assertEqual(self.control.element.label, 'Email Address')
+        self.assertEqual(self.control.element.hint, 'Email field with validation')
+
+        # Doesn't exist, but shouldn't raise Exception
+        self.assertEqual(self.control.element.alert, None)
+
+    def test_builder_form_default_value(self):
+        self.assertEqual(self.control.default_raw_value, 'info@orbeon.com')
+        self.assertEqual(self.control.default_value, 'info@orbeon.com')
+
+    def test_runner_value(self):
+        self.assertEqual(self.runner.get_value('input'), 'John')
+
+    def test_runner_form(self):
+        self.assertEqual(self.runner.form.email.label, 'Email Address')
+        self.assertEqual(self.runner.form.email.value, 'bob@novacode.nl')
