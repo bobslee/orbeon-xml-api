a684259df442346979daac8e6a0b84781acfec26
Bob Leers <b.leers@open2bizz.nl>
2017-10-09 11:02:47 +0200

    Re-version to 0.1.0.dev1 - Because it's not released yet.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='orbeon-xml-api',
-    version='1.0.0.dev6',
+    version='0.1.0.dev1',
     description='A Python object API for Orbeon XML',
     url='https://github.com/bobslee/orbeon-xml-api',
     author='Bob Leers',
