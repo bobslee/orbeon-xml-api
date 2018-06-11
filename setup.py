0287bb3c4e00d7f71aea5a85f3ae27819d311de7
Bob Leers <bob@novacode.nl>
2018-06-11 17:56:01 +0200

    Bump version 27.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='orbeon-xml-api',
-    version='0.1.0.dev26',
+    version='0.1.0.dev27',
     description='A Python object API for Orbeon XML',
     url='https://github.com/bobslee/orbeon-xml-api',
     author='Bob Leers',
